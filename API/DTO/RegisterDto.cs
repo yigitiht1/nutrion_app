d672bac7b041d246fabb8e0b13fee49e990b6c3a
Yiğit İhtiyar <yigit.ihtiyar1@gmail.com>
2025-05-14 17:19:31 +0300

    Update

--- API/DTO/RegisterDto.cs
+++ API/DTO/RegisterDto.cs
@@ -2,8 +2,8 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
     }
 }
