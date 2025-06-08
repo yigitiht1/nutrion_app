4bf98a6490e2024e3bb3de0c90fae6316e619ba6
Yiğit İhtiyar <yigit.ihtiyar1@gmail.com>
2025-06-08 17:41:23 +0300

    .

--- API/Services/IFoodService.cs
+++ API/Services/IFoodService.cs
@@ -3,6 +3,7 @@ using API.Models;
 public interface IFoodService
 {
     Task<List<FoodDto>> GetAllFoodsAsync(); // DTO dönmeli
-    Task<List<FoodDto>> GetFoodsByMealTypeAsync(MealType mealType);
+    // IFoodService implementasyonu zaten yukarıdaki metod
+    public Task<List<FoodDto>> GetFoodsByMealTypeAsync(MealType mealType);
     Task AddFoodAsync(FoodDto foodDto);
 }
