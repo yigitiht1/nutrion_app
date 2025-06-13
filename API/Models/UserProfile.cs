73747f3c1133c8766140bd2e08dfc0aeeb1ce772
Yiğit İhtiyar <yigit.ihtiyar1@gmail.com>
2025-06-13 09:14:43 +0300

    .

--- API/Models/UserProfile.cs
+++ API/Models/UserProfile.cs
@@ -5,18 +5,18 @@ public class UserProfile
 {
     public int Id { get; set; }
     public int UserId { get; set; }
-    public string? UserName { get; set; } 
+    public string UserName { get; set; } 
     public double Height { get; set; }
     public double Weight { get; set; }
     public int Age { get; set; }
-    public string? Gender { get; set; }
+    public string Gender { get; set; }
 
     public double TargetWeight { get; set; }   
     public int TargetDays { get; set; }  
-    public int? CalorieDifference { get; set; }
+    public int  CalorieDifference { get; set; }
          
     [ForeignKey("UserId")]
-    public User? User { get; set; }
+    public User  User { get; set; }
 
 
 
